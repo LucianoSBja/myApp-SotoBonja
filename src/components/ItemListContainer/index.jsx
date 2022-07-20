41ab4742d472dae36589febbdc407969da5b6d37
Luciano Ezequiel Soto Bonja <lucianosotobonja95@gmail.com>
2022-07-20 20:56:29 -0300

    maps_y_promises

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Title from '../Title';
 import ItemCount from '../ItemCount';
+import ItemList from '../ItemList';
 
-export const ItemListContainer = (props) => {
+const films = [
+	{
+		id: 1,
+		img: 'https://http2.mlstatic.com/D_NQ_NP_953586-MCO45131846536_032021-O.webp',
+		title: 'Diadema de lazo',
+	},
+	{
+		id: 2,
+		img: 'https://http2.mlstatic.com/D_NQ_NP_901605-MCO46656717643_072021-O.webp',
+		title: 'Diadema tejida',
+	},
+	{
+		id: 3,
+		img: 'https://http2.mlstatic.com/D_NQ_NP_847602-MCO47701476325_092021-O.webp',
+		title: 'Diadema de tela',
+	},
+];
+
+export const ItemListContainer = ({ texto }) => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const getData = new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(films);
+			}, 3000);
+		});
+		getData.then((res) => setData(res));
+	}, []);
+
 	const onAdd = (quantity) => {
 		console.log(`Compraste ${quantity} unidades`);
 	};
 
 	return (
 		<>
-			<Title greeting="Chau" />
+			<Title greeting={texto} />
 			<ItemCount initial={1} stock={5} onAdd={onAdd} />
+			<ItemList data={data} />
 		</>
 	);
 };
